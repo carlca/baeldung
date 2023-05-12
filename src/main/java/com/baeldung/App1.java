1283a5560f4a18c0e99563a2f571a3743d3080ff
carlca <carl.caulkett@gmail.com>
2023-05-12 22:56:27 +0100

    mrun App1 + mrun App2 FTW

---
+++ src/main/java/com/baeldung/App1.java
@@ -0,0 +1,13 @@
+package com.baeldung;
+
+/**
+ * Hello world!
+ *
+ */
+public class App1
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! from App 1" );
+    }
+}
